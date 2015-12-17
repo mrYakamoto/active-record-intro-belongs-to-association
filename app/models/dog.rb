0e67734ac0f4c82b40371fe8e66315681fe297e1
Mia Lehrer & Walter Kerr <pair+mllehrer.walterkerr@devbootcamp.com>
2015-12-17 11:49:05 -0800

    Adds final Active Record Intro files

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,3 @@
 class Dog < ActiveRecord::Base
+  belongs_to :owner, { :class_name => "Person", :foreign_key => :owner_id }
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,5 @@
 class Rating < ActiveRecord::Base
   belongs_to :dog
+  belongs_to :judge, { :class_name => "Person", :foreign_key => :judge_id }
 end
+
